8d466e5f6f1b2106ffa700d7d3c3e75f0499bd25
Wonsang Song <wonsang@cs.columbia.edu>
2012-10-04 19:43:33 -0400

    Initial Commit

---
+++ Indopos.ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Indopos.ios
+//
+//  Created by Wonsang Song on 10/4/12.
+//  Copyright (c) 2012 Wonsang Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Indopos.ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Indopos.ios
+//
+//  Created by Wonsang Song on 10/4/12.
+//  Copyright (c) 2012 Wonsang Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
